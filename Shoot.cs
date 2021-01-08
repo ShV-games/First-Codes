94f5bd5466cfe10c8699855cfcb471c3365c5af9
ShV-games <70167596+ShV-games@users.noreply.github.com>
2021-01-08 22:19:54 +0100

    Add files via upload

---
+++ Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform Bulletspawn;
+    Rigidbody2D clone;
+    public Rigidbody2D bulletPrefap;
+    public float bulletSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Attack()
+    {
+        clone = Instantiate(bulletPrefap, Bulletspawn.position, Bulletspawn.rotation);
+        clone.AddForce(Bulletspawn.transform.right * bulletSpeed);
+
+    }
+}
